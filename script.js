9adc10affa6c2601002fb473f93b250bd1f384d1
Ruan.Miniguite <ruanminiguite@gmail.com>
2021-07-19 21:04:45 -0300

    :package: tela de Loader

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
 
+const overlay = document.querySelector(".overlay");
+
+window.addEventListener("load", setTimeout(function () {
+    overlay.style.display = "none";
+}, 1000))
+
 //document.oncontextmenu = document.body.oncontextmenu = function() {return false;}
