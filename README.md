2e2bd1bf041f147461a044623cbf499842230c38
Ruan.Miniguite <ruanminiguite@gmail.com>
2021-07-21 03:38:57 -0300

    :checkered_flag: 1º release

--- README.md
+++ README.md
@@ -1,11 +1,31 @@
-# Portfólio
+## Portfólio
 
-Meu Portfólio de desenvolvedor 
+[![Website](https://img.shields.io/website?down_color=181717&down_message=404&style=for-the-badge&up_color=181717&up_message=live&url=https%3A%2F%2Fruanminiguite.github.io)](https://ruanminiguite.github.io/)
+---
 
-## Ícones
+### ABOUT
+Portfólio pessoal de desenvolvedor, em construção!
+
+### LANGUAGE AND TOOLS
+#### FRONT END
+
+![HTML5](https://img.shields.io/badge/-HTML5-181717?style=flat-square&logo=html5&logoColor=ffffff)
+![JavaScript](https://img.shields.io/badge/-JavaScript-%23F7DF1C?style=flat-square&logo=javascript&logoColor=FFFFFF&labelColor=181717&color=181717)
+![CSS3](https://img.shields.io/badge/-CSS3-181717?style=flat-square&logo=css3)
+
+#### BACK END
+
+![JavaScript](https://img.shields.io/badge/-JavaScript-%23F7DF1C?style=flat-square&logo=javascript&logoColor=FFFFFF&labelColor=181717&color=181717)
+
+### COMMIT ICON
 
 - :package: nova funcionalidade
 - :up: atualização
 - :beetle: correção de bug
 - :checkered_flag: release
 
+### AUTHOR
+
+Ruan Pezzin Miniguite
+
+[![NPM](https://img.shields.io/cocoapods/l/m?style=for-the-badge)](https://github.com/RuanMiniguite/RuanMiniguite.github.io/blob/d686305b8c6f01f55044efcb93ab6929bdc18c89/LICENSE)
--- script.js
+++ script.js
@@ -1,54 +1,44 @@
+//sidebar
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
 
+//Loader
 const overlay = document.querySelector(".overlay");
 
 window?.addEventListener("load", function () {
     overlay.style.display = "none";
 });
 
-
+//email
 const send = async () => {
   event.preventDefault();
-
-  var myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  myHeaders.append( "Access-Control-Allow-Origin", "*");
-   
-  var raw = JSON.stringify({
-    "email": "RuanMiniguite@gmail.com",
-    "name": "Ruan",
-    "msg": "portifolio"
-  });
-
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
-  };
-
-fetch("https://ruanminiguite.wixsite.com/my-site/_functions/enviarEmail", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-
-
+  
   let nome = document.getElementById("name").value;
   let email = document.getElementById("email").value;
-  let msg = document.getElementById("message").value;
-  
+
+ const response = await fetch('https://ruanminiguite.wixsite.com/my-site/_functions/enviarEmail?email='+ email +'&name=' + nome + '', {
+      method: 'GET', 
+      headers: {
+        'User-Agent': 'PostmanRuntime/7.28.2',
+        'Accept': '*/*',
+        'Accept-Encoding':'gzip, deflate, br',
+        'Connection': 'keep-alive'
+      }
+    });
+    const myJson = await response.json;
+    
+    if(response.status === 200){
+      alert("Message send! \n\nWe will be in touch! 🙂")
+    }else{
+      alert("Error sending message!\n\nTry again later! 🙃")
+    }
+
+  limpar();
+   
 }
-  // const response = await fetch('https://ruanminiguite.wixsite.com/my-site/_functions/enviarEmail', {
-  //     method: 'POST',
-  //     body: JSON.stringify({
-  //       "email" : email,
-  //       "name" : nome,
-  //       "msg" : msg
-  //     }), 
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   });
-  //   const myJson = await response.json; 
+
+function limpar() {
+  document.getElementById('name').value = "";
+  document.getElementById('email').value = "";
+}
