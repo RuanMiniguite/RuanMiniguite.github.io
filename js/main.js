e98a5a4f001886e1f8e26e2454a07c12239f9a3d
RuanMiniguite <Ruanminiguite@gmail.com>
2023-06-28 20:29:21 -0300

    :art: Icon mouse scroll

--- js/main.js
+++ js/main.js
@@ -2,3 +2,16 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+// icone mouse apagar quando pagina sair do topo
+window.onscroll = function () {
+    scrollFunction();
+}
+
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        document.querySelector("#I-mouse").style.display = "none";
+    } else {
+        document.querySelector("#I-mouse").style.display = "block";
+    }
+}
